3646a33f22eaa55df5dc1086413195bc53ddcd85
takayuki <takayuki.sato.dev@gmail.com>
2014-07-03 14:10:56 +0900

    fix indentation

--- src/cl-ltsv.lisp
+++ src/cl-ltsv.lisp
@@ -20,32 +20,31 @@
 
 (defun split-sequence (c s)
   (loop
-     for right = (position-if-c c s) then (position-if-c c s :start (1+ right))
-     and left = 0 then (1+ right)
-     collect (subseq s left right)
-     until (null right)))
-    
+   for right = (position-if-c c s) then (position-if-c c s :start (1+ right))
+   and left = 0 then (1+ right)
+   collect (subseq s left right)
+   until (null right)))
+
 (defun parse-line (s)
   (loop
-     for kv in (split-sequence #\tab s)
-     collect (let ((pos (position-if-c #\: kv)))
-	       (if pos
-		   (cons (subseq kv 0 pos)
-			 (subseq kv (1+ pos)))))))
+   for kv in (split-sequence #\tab s)
+   collect (let ((pos (position-if-c #\: kv)))
+             (if pos
+                 (cons (subseq kv 0 pos)
+                       (subseq kv (1+ pos)))))))
 
 (defmacro with-ltsv-from-stream ((entry stream) &body body)
   (let ((line (gensym)))
     `(loop
-        for ,line = (read-line ,stream nil :eof)
-        until (eql ,line :eof)
-        do (let ((,entry (parse-line ,line)))
-             ,@body))))
+      for ,line = (read-line ,stream nil :eof)
+      until (eql ,line :eof)
+      do (let ((,entry (parse-line ,line))) ,@body))))
 
 (defun alist-ltsv (entry)
   (with-output-to-string (ss)
-    (loop
-       for alist in entry
-       and _ = nil then (format ss "	")
-       do (format ss "~A:~A" (car alist) (cdr alist)))))
+                         (loop
+                          for alist in entry
+                          and _ = nil then (format ss "	")
+                          do (format ss "~A:~A" (car alist) (cdr alist)))))
 
-       
+
